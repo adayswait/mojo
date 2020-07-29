b441a86030668e015703327ae48d6b62263c598f
adayswait <adayswait@139.com>
2020-07-29 16:47:02 +0800

    add uuid

--- svr/go.mod
+++ svr/go.mod
@@ -4,8 +4,8 @@ require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/gofiber/cors v0.2.1
 	github.com/gofiber/fiber v1.13.1
-	github.com/gofiber/session v1.2.4
 	github.com/gofiber/websocket v0.5.0
+	github.com/google/uuid v1.1.1
 )
 
 go 1.14
--- svr/handler/api.go
+++ svr/handler/api.go
@@ -3,13 +3,14 @@ package handler
 import "time"
 import "fmt"
 import "github.com/gofiber/fiber"
+import "github.com/google/uuid"
 
 func Hello(c *fiber.Ctx) {
 	fmt.Println(c.Cookies("jesse"))
 	c.JSON(fiber.Map{"status": "success", "message": "Hello i'm ok!", "data": nil})
 	cookie := new(fiber.Cookie)
 	cookie.Name = "jesse"
-	cookie.Value = "sdajsdsahjdkhsakd"
+	cookie.Value = uuid.New().String()
 	cookie.Expires = time.Now().Add(24 * time.Hour)
 	c.Cookie(cookie)
 }
