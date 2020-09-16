b827acefa8d54e2022a9832a6cdf9634844cb8e2
adayswait <adayswait@139.com>
2020-09-16 16:02:34 +0800

    change deploy script for self-hosted deployment

--- ar.sh
+++ ar.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+cd `dirname "$0"`
+pidlist=$(pidof $(pwd)/mojo)
+pidnum=${#pidlist[*]}
+if [ ${pidnum} -gt 0 ]; then
+	for svrpid in ${pidlist}
+	do
+		kill ${svrpid}
+	done
+fi
 alias error=">&2 echo counter: "
 # 以bash this.sh执行
 cd `dirname "$0"`
--- br.sh
+++ br.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 cd `dirname "$0"`
-pidlist=$(pidof $(pwd)/mojo)
-pidnum=${#pidlist[*]}
-if [ ${pidnum} -gt 0 ]; then
-	for svrpid in ${pidlist}
-	do
-		kill ${svrpid}
-	done
-fi
+rm ./mojo
 echo mojobrok
